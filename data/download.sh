dcee311733c2f3bd3a5acc8c783ee6013ce0b3f8
Xiaozhi Wang <wangxz20@mails.tsinghua.edu.cn>
2022-11-03 20:02:37 +0800

    add back data

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+# download processed data
+wget -O processed.zip https://cloud.tsinghua.edu.cn/f/90aac3ad359f4e3e9082/?dl=1
+unzip processed.zip
---
+++ data/download_maven.sh
@@ -0,0 +1,2 @@
+wget -O MAVEN_ERE.zip https://cloud.tsinghua.edu.cn/f/a7d1db6c44ea458bb6f0/?dl=1
+unzip MAVEN_ERE.zip
